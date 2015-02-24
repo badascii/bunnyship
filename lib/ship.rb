050b404e39972e4866c1b65fbdba0c854c1a1a9a
Jordan A. Skarbek <jaskarbek@gmail.com>
2015-02-24 14:36:35 -0800

    added Ship#neighboring_positions? and tests for it

--- lib/ship.rb
+++ lib/ship.rb
@@ -24,4 +24,22 @@ class Ship
     end
   end
 
+  def in_sequence?
+    positions.each do |position|
+      # [{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 5}]
+      positions.each do |pos|
+        if pos[:x] != position[:x]
+          if (((position[:x] - 1) == pos[:x]) || ((position[:x] + 1) == pos[:x])) && ((position[:y] == pos[:y]) || (position[:y] == pos[:y]))
+            return true
+          end
+        end
+      end
+    end
+    return false
+  end
+
+  def neighboring_positions?(pos_1, pos_2)
+    (pos_1 - 1 == pos_2) || (pos_1 + 1 == pos_2)
+  end
+
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -24,12 +24,33 @@ class ShipTest < MiniTest::Test
     assert_equal(false, @ship.shot_hit?(miss))
   end
 
-  def test_destroyed
+  def test_damage_ordered_destroyed
     assert_equal(false, @ship.destroyed?)
 
     @ship.damage = [{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}]
 
     assert_equal(true, @ship.destroyed?)
+  end
+
+  def test_in_sequence
+    opts = {
+      type:      'cruiser',
+      positions: [{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}],
+      damage:    [{x: 1, y: 1}]
+    }
+    unordered_ship = Ship.new(opts)
+
+    assert_equal(false, unordered_ship.in_sequence?)
+    assert_equal(true, @ship.in_sequence?)
+  end
+
+  def test_neighboring_positions
+    pos_1 = 1
+    pos_2 = 2
+    pos_3 = 5
+
+    assert_equal(true, @ship.neighboring_positions?(pos_1, pos_2))
+    assert_equal(false, @ship.neighboring_positions?(pos_1, pos_3))
   end
 
 end
