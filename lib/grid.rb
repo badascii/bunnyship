09123c05331fa138b56efc165e0ee225a5e46c3d
Neil Northrop <nnorthrop@gmail.com>
2015-03-02 15:02:10 -0800

    fix spacing

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,10 @@
 require 'set'
 
 class Grid
-	attr_reader :height, :width
+  attr_reader :height, :width
 
-	def initialize(opts={})
-		@height = opts[:height] || 10
-		@width 	= opts[:width] 	|| 10
-	end
+  def initialize(opts={})
+    @height = opts[:height] || 10
+    @width  = opts[:width]  || 10
+  end
 end
