0a998345205094f93ab7641da9d7b793e42f9db8
Jordan A. Skarbek <jaskarbek@gmail.com>
2015-05-06 16:18:58 -0700

    updated player worker run file to use separate channels for each exchange

--- test/start_player_worker.rb
+++ test/start_player_worker.rb
@@ -8,23 +8,24 @@ conn = Bunny.new(automatically_recover: false)
 
 conn.start
 
-ch                  = conn.create_channel
-player_rpc_worker   = PlayerRPCWorker.new(ch, 'setup_queue')
-player_topic_worker = PlayerTopicWorker.new(ch)
+rpc_ch              = conn.create_channel
+topic_ch            = conn.create_channel
+player_rpc_worker   = PlayerRPCWorker.new(rpc_ch, 'setup_queue')
+player_topic_worker = PlayerTopicWorker.new(topic_ch)
 
 input_hash = {}
-response = ''
+response   = ''
 
 response = if ARGV[0] == 'play'
-             input = ['play', 'Jimmy']
+             input      = ['play', 'Jimmy']
              input_hash = player_rpc_worker.build_play_input_hash(input)
              player_rpc_worker.rpc_call(input_hash)
            elsif ARGV[0] == 'place'
-             input = ['place', 'battleship', '1', '1', 'v', 'Jimmy', ARGV[1]]
+             input      = ['place', 'battleship', '1', '1', 'v', 'Jimmy', ARGV[1]]
              input_hash = player_rpc_worker.build_placement_input_hash(input)
              player_rpc_worker.rpc_call(input_hash)
            elsif ARGV[0] == 'ready'
-             input = ['ready', 'Jimmy', ARGV[1]]
+             input      = ['ready', 'Jimmy', ARGV[1]]
              input_hash = player_rpc_worker.build_ready_input_hash(input)
              player_rpc_worker.rpc_call(input_hash)
            end
@@ -45,5 +46,6 @@ end
 
 puts " [.] Got #{response}"
 
-ch.close
+rpc_ch.close
+topic_ch.close
 conn.close
