5d126f5ebca3f0c7225e86ec52a936079748b658
Jordan A. Skarbek <jaskarbek@gmail.com>
2015-05-06 15:58:54 -0700

    added separate channels for rpc/topic exchanges and added ready command processing.

--- test/start_player_worker.rb
+++ test/start_player_worker.rb
@@ -10,37 +10,40 @@ conn.start
 
 ch                  = conn.create_channel
 player_rpc_worker   = PlayerRPCWorker.new(ch, 'setup_queue')
-player_topic_worker = PlayerTopicWorker.new(ch, 'update_queue')
+player_topic_worker = PlayerTopicWorker.new(ch)
 
 input_hash = {}
+response = ''
 
-if ARGV[0] == 'play'
-  input = ['play', 'Jimmy']
-  input_hash = player_rpc_worker.build_play_input_hash(input)
-elsif ARGV[0] == 'place'
-  input = ['place', 'battleship', '1', '1', 'v', 'Jimmy', ARGV[1]]
-  input_hash = player_rpc_worker.build_placement_input_hash(input)
-elsif ARGV[0] == 'ready'
-  input = ['ready', 'Jimmy', ARGV[1]]
-  input_hash = player_rpc_worker.build_ready_input_hash(input)
-end
+response = if ARGV[0] == 'play'
+             input = ['play', 'Jimmy']
+             input_hash = player_rpc_worker.build_play_input_hash(input)
+             player_rpc_worker.rpc_call(input_hash)
+           elsif ARGV[0] == 'place'
+             input = ['place', 'battleship', '1', '1', 'v', 'Jimmy', ARGV[1]]
+             input_hash = player_rpc_worker.build_placement_input_hash(input)
+             player_rpc_worker.rpc_call(input_hash)
+           elsif ARGV[0] == 'ready'
+             input = ['ready', 'Jimmy', ARGV[1]]
+             input_hash = player_rpc_worker.build_ready_input_hash(input)
+             player_rpc_worker.rpc_call(input_hash)
+           end
 
 puts " [x] Sent input"
 
-response = player_rpc_worker.rpc_call(input_hash)
+if ARGV[0] == 'fire'
+  input_hash = {
+    target:  'Jimmy',
+    name:    'Jimmy',
+    x:       1,
+    y:       1,
+    game_id: ARGV[1]
+  }
+
+  response = player_topic_worker.emit_shot(input_hash)
+end
 
 puts " [.] Got #{response}"
-
-# input_hash = {
-#   command: 'fire',
-#   payload: {
-#     target: 'Jimmy',
-#     x:      1,
-#     y:      1,
-#     name:   'Jimmy',
-#     game_id: "#{response}"
-#   }
-# }
 
 ch.close
 conn.close
